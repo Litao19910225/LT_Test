c92854df05f25a199d725b9d9a177e44e10f2570
tao.li28 <tao.li28@pactera-fintech.com>
2020-11-10 16:21:28 +0800

    添加sdk

---
+++ LT_TestSDK.framework/Headers/LT_TestSDK.h
@@ -0,0 +1,19 @@
+//
+//  LT_TestSDK.h
+//  LT_TestSDK
+//
+//  Created by 涛胖子 on 2020/11/10.
+//  Copyright © 2020 LT_TestSDK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LT_TestSDK.
+FOUNDATION_EXPORT double LT_TestSDKVersionNumber;
+
+//! Project version string for LT_TestSDK.
+FOUNDATION_EXPORT const unsigned char LT_TestSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LT_TestSDK/PublicHeader.h>
+
+
